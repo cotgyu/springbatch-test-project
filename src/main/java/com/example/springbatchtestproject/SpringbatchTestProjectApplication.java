7940c3a6d4514c49aac48ab8357751cd77a66358
cotgyu <v123v123s@naver.com>
2021-01-10 00:38:39 +0900

    Initial commit

---
+++ src/main/java/com/example/springbatchtestproject/SpringbatchTestProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbatchtestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringbatchTestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbatchTestProjectApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/springbatchtestproject/job/SimpleJobConfiguration.java
@@ -0,0 +1,4 @@
+package com.example.springbatchtestproject.job;
+
+public class SimpleJobConfiguration {
+}
